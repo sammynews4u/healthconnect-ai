e55f598d633271a8c444a7f01333bf8a5d2c44aa
Samuel Adesanya <samuel.adesanya1love@gmail.com>
2025-12-26 19:20:41 +0100

    Fix TypeScript node environment issues

---
+++ types.js
@@ -0,0 +1,13 @@
+export var UserRole;
+(function (UserRole) {
+    UserRole["PATIENT"] = "PATIENT";
+    UserRole["NURSE"] = "NURSE";
+    UserRole["DOCTOR"] = "DOCTOR";
+    UserRole["ADMIN"] = "ADMIN";
+})(UserRole || (UserRole = {}));
+export var ConsultationType;
+(function (ConsultationType) {
+    ConsultationType["CHAT"] = "CHAT";
+    ConsultationType["AUDIO"] = "AUDIO";
+    ConsultationType["VIDEO"] = "VIDEO";
+})(ConsultationType || (ConsultationType = {}));
